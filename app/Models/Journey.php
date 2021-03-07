b19fe1d90af1ddcf742e52de6b69877e269e2521
Jake Readman <jakereadman@gmail.com>
2021-03-07 14:08:32 +0000

    create origins and destination arrays and find start point

--- app/Models/Journey.php
+++ app/Models/Journey.php
@@ -7,10 +7,30 @@
 
         //extract file contents and convert from json
         function __construct($file) {
-            //try / catch test for json file with correct schema input
+            //try / catch test for json file with correct schema input?
             $this->input = file_get_contents($file);
             $this->decodedArray = json_decode($this->input, true);
         }
+
+        //Create origins and destinations arrays
+        private function originsAndDestinations($decodedArray) {
+            foreach($decodedArray as $arr) {
+                array_push($origins, $arr['from']);
+                array_push($destinations, $arr['to']);
+            }       
+        }
+
+        //Find unique origin to get start point
+        private function setStartPoint($origins, $destinations) {
+            foreach($this->origins as $origin) {
+                if(!in_array($origin, $this->destinations)) {
+                    array_push($this->final, $origin);
+                    $this->start = $origin;
+                }
+            }
+
+        }
+
     }
 
 ?>
